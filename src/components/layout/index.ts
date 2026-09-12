87beb30688948088abe2e89ff30dacdbfa50f7cc
Dewa M. Widyakumara <widyakumara@users.noreply.github.com>
2026-09-13 00:25:50 +0700

    chore: reset (#9)

--- src/components/layout/index.ts
+++
@@ -1,7 +0,0 @@
-import Page from "./page.astro";
-import PageHead from "./page.head.astro";
-import PageMenu from "./page.menu.astro";
-import PageMeta from "./page.meta.astro";
-import PageSide from "./page.side.astro";
-
-export { Page, PageHead, PageMenu, PageMeta, PageSide };
